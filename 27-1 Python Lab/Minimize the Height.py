62957e272664860466983ca3bda2b891ed1d2aff
shauryaiilm <shaurya.srivastava.cs28@iilm.edu>
2026-02-10 16:56:40 +0530

    Create Minimize the Height.py

---
+++ 27-1 Python Lab/Minimize the Height.py
@@ -0,0 +1,16 @@
+def getMinDiff(self, arr, k):
+        n = len(arr)
+        arr.sort()
+        
+        ans = arr[n-1] - arr[0]
+        
+        for i in range(1, n):
+            if arr[i] - k < 0:
+                continue
+            
+            min_height = min(arr[0] + k, arr[i] - k)
+            max_height = max(arr[i-1] + k, arr[n-1] - k)
+            
+            ans = min(ans, max_height - min_height)
+        
+        return ans
